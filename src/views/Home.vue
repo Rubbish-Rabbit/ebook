10c7dbda69901848c4e9ee1e85e1907ad55d4b4f
Rubbish-Rabbit <1305237417@qq.com>
2019-04-03 20:48:41 +0800

    初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <span class="icon-bookmark"></span>
+    <span class="icon-clock"></span>
+  </div>
+</template>
+
+<script>
+  import Epub from 'epubjs'
+  global.ePub = Epub
+  export default {
+  }
+</script>
+
+<style scoped>
+  .icon-bookmark{
+    font-size:80px;
+    color: red;
+  }
+</style>
